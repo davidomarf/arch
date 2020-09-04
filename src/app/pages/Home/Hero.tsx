858699193ec7d6ddb7f111e654fc4d2a5203608c
David Omar Flores Chávez <david@davidomar.com>
2020-09-04 02:29:12 -0500

    Separate Hero into a different component

---
+++ src/app/pages/Home/Hero.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import styles from "./Hero.module.scss";
+import { Link } from "react-router-dom";
+
+const Hero = () => (
+    <div className={styles.hero}>
+      <img src="https://images.unsplash.com/photo-1593642533144-3d62aa4783ec?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"></img>
+      <HeroDescription
+        title="Project Paramour"
+        description="Project made for an art museum near Southwest London. Project Paramour is a statement of bold, modern architecture."
+      />
+    </div>
+);
+
+type HeroDescriptionProps = {
+  title: string;
+  description: string;
+};
+const HeroDescription = ({ title, description }: HeroDescriptionProps) => (
+  <div className={styles.heroDescription}>
+    <h2>{title}</h2>
+    <p>{description}</p>
+    <Link to="/portfolio" className={styles.portfolio}>
+      See Our Portfolio -->
+    </Link>
+  </div>
+);
+
+export default Hero;
