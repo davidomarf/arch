5b1c8280b74fbbbcd705db6027f513fe2b854e73
David Omar Flores Chávez <david@davidomar.com>
2020-09-04 01:07:27 -0500

    Create a Pages directory and create components that render on routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,10 @@
 import React from "react";
-import Layout from "./Layout/Layout";
-
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Layout from "./Layout/Layout";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import Portfolio from "./pages/Portfolio/Portfolio";
+
 function App() {
   return (
     <Router>
@@ -9,9 +12,15 @@ function App() {
         <Route path="/" exact>
           Home
         </Route>
-        <Route path="/about">About</Route>
-        <Route path="/portfolio">POrtfolio</Route>
-        <Route path="/contact"> Contact</Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/portfolio">
+          <Portfolio />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
       </Layout>
     </Router>
   );
