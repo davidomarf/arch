ebf35d58f36a059597137a57636347416d93de11
David Omar Flores Chávez <david@davidomar.com>
2020-09-04 01:15:59 -0500

    Replace a-tags with Link components from react-router

--- src/app/Layout/Navbar.tsx
+++ src/app/Layout/Navbar.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import styles from "./Navbar.module.scss";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <nav id={styles.mainNavigation}>
       <div className={styles.home}>
-        <a href="/">Arch</a>
+        <Link to="/">Arch</Link>
       </div>
       <div className={styles.sections}>
-        <a href="/portfolio">Portfolio</a>
-        <a href="/about">About us</a>
-        <a href="/contact">Contact</a>
+        <Link to="/portfolio">Portfolio</Link>
+
+        <Link to="/about">About us</Link>
+
+        <Link to="/contact">Contact</Link>
       </div>
     </nav>
   );
