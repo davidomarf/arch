42c3458cf26670b45bfd5a5bff25b93ef4fb87b5
David Omar Flores Chávez <david@davidomar.com>
2020-09-04 03:41:41 -0500

    Import, style, and use the arrow for the See Our Portfolio element

--- src/app/Layout/Footer.tsx
+++ src/app/Layout/Footer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./Footer.module.scss";
 import { Link } from "react-router-dom";
 import { ReactComponent as ArchLogo } from "../assets/img/logo.svg";
+import { ReactComponent as ArrowIcon } from "../assets/img/icons/icon-arrow.svg";
 
 const Footer = () => {
   return (
@@ -17,7 +18,7 @@ const Footer = () => {
         <Link to="/contact">Contact</Link>
       </div>
       <Link to="/portfolio" className={styles.portfolio}>
-        See Our Portfolio --
+        See Our Portfolio <ArrowIcon />
       </Link>
     </div>
   );
