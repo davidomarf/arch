99a2b7a09dc3ee312d1e963c3bc8ce65898314a1
David Omar Flores Chávez <david@davidomar.com>
2020-09-04 01:39:42 -0500

    Add a useLocation hook to detect the current route and style the link

--- src/app/Layout/Navbar.tsx
+++ src/app/Layout/Navbar.tsx
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { PropsWithChildren } from "react";
 import styles from "./Navbar.module.scss";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const NavBar = () => {
+  let { pathname } = useLocation();
+
   return (
     <nav id={styles.mainNavigation}>
       <div className={styles.home}>
         <Link to="/">Arch</Link>
       </div>
       <div className={styles.sections}>
-        <Link to="/portfolio">Portfolio</Link>
+        <Link to="/portfolio" className={getClass(pathname, "/portfolio")}>
+          Portfolio
+        </Link>
 
-        <Link to="/about">About us</Link>
+        <Link to="/about" className={getClass(pathname, "/about")}>
+          About us
+        </Link>
 
-        <Link to="/contact">Contact</Link>
+        <Link to="/contact" className={getClass(pathname, "/contact")}>
+          Contact
+        </Link>
       </div>
     </nav>
   );
 };
+
+function getClass(path: string, current: string) {
+  return path === current ? styles.active : "";
+}
 
 export default NavBar;
